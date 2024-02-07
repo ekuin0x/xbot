6706521813fdb3da41faa7a1104f08b89e1a6137
ekuin0x <salhimouiz1@gmail.com>
2024-02-07 22:16:41 +0100

    jjj

--- comment_bot.py
+++ comment_bot.py
@@ -2,44 +2,65 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys  
-import time
+from time import sleep
+import random
+import json
+
+comments = [
+    "Wanna see my n*d! pics? https://shorturl.at/DTX59",
+    "I'm going ho#ny here https://shorturl.at/DTX59",
+    "Special offers for my baby sugars https://shorturl.at/DTX59"
+]
 
 def comment(userId, password, keyword, mssg) :
+    i=0
     browser = webdriver.Chrome()
     browser.get("https://twitter.com/i/flow/login")
-    time.sleep(5)
+    sleep(5)
     userIdField = browser.find_element(By.CSS_SELECTOR, "input").send_keys(userId)
-    time.sleep(2)
+    sleep(5)
     browser.find_elements(By.CSS_SELECTOR, "div[role='button']")[2].send_keys(Keys.ENTER)
-    time.sleep(2)
+    sleep(5)
     passwordField = browser.find_element(By.CSS_SELECTOR, "input[type='password']").send_keys(password)
-    time.sleep(2)
+    sleep(5)
     browser.find_elements(By.CSS_SELECTOR, "div[role='button']")[2].send_keys(Keys.ENTER)
-    time.sleep(7)
-    browser.find_element(By.CSS_SELECTOR, "input[type='text']").send_keys(Keys.ENTER)
-    time.sleep(2)
+    sleep(20)
+    browser.find_element(By.CSS_SELECTOR, "input[type='text']").send_keys(keyword)
+    sleep(4)
     browser.find_element(By.CSS_SELECTOR, "form").submit()
-    time.sleep(7)
+    sleep(7)
     
     articles = browser.find_elements(By.CSS_SELECTOR, "article")
-    time.sleep(10)
+    sleep(10)
     scrollTo = 0
-    for article in articles :
+
+
+    for i in range(len(articles)) :
         browser.execute_script(f"window.scrollTo(0, {str(scrollTo)})")
-        article.click()
-        time.sleep(10)
+        articles[i].click()
+        sleep(10)
         browser.find_element(By.CSS_SELECTOR, "div[contenteditable='true']").send_keys(mssg)
-        time.sleep(10)
+        sleep(10)
         browser.find_element(By.CSS_SELECTOR, "div[data-testid='tweetButtonInline']").click()
-        time.sleep(10)
+        sleep(10)
         browser.find_element(By.CSS_SELECTOR, "main div[aria-label='Back']").click()
         scrollTo = int(scrollTo) + 700
-        time.sleep(10)
+        sleep(10)
+        browser.find_element(By.CSS_SELECTOR, "form").submit()
+        sleep(7)
+        articles = browser.find_elements(By.CSS_SELECTOR, "article")
+        time.sleep(400)
+        i+=1
 
     driver.quit()
 
-keyword = "Dating"
-mssg = "Hello World"
-comment("AFabicci65289","s)H4ZJvbbe6Uv2x",keyword,mssg)
+with open("auth.json", 'r') as f :
+    data = json.loads(f.read())
+    for account in data : 
+        user = account["userId"]
+        psswrd = account["password"]
+        cmnt = random.choice(comments)
+        comment(user,psswrd,"dating",cmnt)
+
 
 
